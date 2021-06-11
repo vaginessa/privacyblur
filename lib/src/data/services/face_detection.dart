c0a946f93a8ecf34884793b30cde5235757a25dc
Pirmin Bahr <pirmin.bahr@mathema.de>
2021-06-11 14:11:37 +0200

    face detection service

---
+++ lib/src/data/services/face_detection.dart
@@ -0,0 +1,32 @@
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+
+class Face {
+  final double x;
+  final double y;
+  final double radius;
+  Face(this.x, this.y, this.radius);
+}
+
+typedef Faces = List<Face>;
+
+class FaceDetection {
+  static const _platform = const MethodChannel('de.mathema.privacyblur/face_detection');
+
+  FaceDetection._privateConstructor();
+
+  static final FaceDetection _instance = FaceDetection._privateConstructor();
+
+  factory FaceDetection() {
+    return _instance;
+  }
+
+  Future<Faces> getSize(Uint8List rawImageData) async {
+    try {
+      return await _platform.invokeMethod('getFaceDetections', rawImageData);
+    } catch(err) {
+      return Future.value([]);
+    }
+  }
+}

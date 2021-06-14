3d000ff660c9c57f790ea743216708f6a965d9b3
Leonid Arefev <leonid.arefev@mathema.de>
2021-06-14 11:48:07 +0200

    android part

--- android/app/src/main/kotlin/de/mathema/privacyblur/MainActivity.kt
+++ android/app/src/main/kotlin/de/mathema/privacyblur/MainActivity.kt
@@ -1,22 +1,66 @@
 package de.mathema.privacyblur
 
 import androidx.annotation.NonNull
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "de.mathema.privacyblur/memory"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            call, result ->
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
             if (call.method == "getHeapSize") {
                 result.success(Runtime.getRuntime().maxMemory())
-            }else{
+            } else {
                 result.notImplemented()
             }
         }
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "getFaceDetections") {
+                var srcImage = call.argument<ByteArray>("argb8")
+                var width = call.argument<Int>("width")
+                var height = call.argument<Int>("height")
+                // Real-time contour detection
+                val realTimeOpts = FaceDetectorOptions.Builder()
+                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                    .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+                    .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+                    .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+                    .build()
+                var processedImage = InputImage.fromByteArray(
+                    srcImage,
+                    width!!,
+                    height!!,
+                    0,
+                    InputImage.IMAGE_FORMAT_BITMAP
+                )
+                val detector = FaceDetection.getClient(realTimeOpts)
+                val detectionProcess = detector.process(processedImage)
+                    .addOnSuccessListener { faces ->
+                        result.success(faces)
+                    }
+                    .addOnFailureListener { e ->
+                        result.error(e.stackTrace.toString(), e.localizedMessage, e)
+                    }
+                    .addOnCanceledListener {
+                        result.error("canceled","canceled","canceled")
+                    }
+            } else {
+                result.notImplemented()
+            }
+        }
+
     }
 }

3f3ecac79d47a057f619479226a52f78d8baa5c3
Pirmin Bahr <pirmin.bahr@mathema.de>
2021-10-13 11:15:02 +0200

    readd main.dart for macos support

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'src/app_container.dart';
+import 'src/utils/flavors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  BuildFlavor.flavor = Flavor.FULL;
+  runApp(await AppContainer().app);
+}
